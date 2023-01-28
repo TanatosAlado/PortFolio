1cb94b71e5a088e95dbc0634798984acf5aa1c6b
Cristian Alessandria <cristian_alessandria@hotmail.com>
2023-01-28 01:09:09 -0300

    Dando formato

---
+++ src/script.js
@@ -0,0 +1,13 @@
+let idioma = "espanol"
+
+function cambioIdioma(){
+    if(idioma == "espanol"){
+        idioma = "ingles";
+        console.log(`Mutaste a idioma: ${idioma}`)
+
+    }
+    else{
+        idioma = "espanol"
+        console.log(`Mutaste a idioma: ${idioma}`)
+    }
+}
