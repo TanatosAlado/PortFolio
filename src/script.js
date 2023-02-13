df71be68ad789dbaf16303d3202c8a4cfd6f2a11
Cristian Alessandria <cristian_alessandria@hotmail.com>
2023-02-13 11:21:10 -0300

    footer

--- src/script.js
+++ src/script.js
@@ -17,7 +17,26 @@ function cambioIdioma(){
         elIdioma = document.getElementById("sobreMi2").innerHTML = "I am passionate about continuous learning and I enjoy applying new technologies to solve a problem."
         elIdioma = document.getElementById("sobreMi3").innerHTML = "I like new challenges. Currently venturing into the freelance world."
         elIdioma = document.getElementById("proyectos1").innerHTML = "Completed Projects"
-        
+        elIdioma = document.getElementById("proyectos2").innerHTML = "Wine store. Using technologies like ReactJs, CSS, BOOTSTRAP and JavaScript"
+        elIdioma = document.getElementById("proyectos3").innerHTML = "Project link"
+        elIdioma = document.getElementById("proyectos4").innerHTML = "Event site. Using technologies like HTML5, CSS3, BOOTSTRAP and JavaScript"
+        elIdioma = document.getElementById("proyectos5").innerHTML = "Project link"
+        elIdioma = document.getElementById("proyectos6").innerHTML = "Memory game developed using technologies such as HTML, CSS, BOOTSTRAP and JavaScript"
+        elIdioma = document.getElementById("proyectos7").innerHTML = "Project link"
+        elIdioma = document.getElementById("contacto1").innerHTML = "Contact me"
+        elIdioma = document.getElementById("contacto2").innerHTML = "Name"
+        elIdioma = document.getElementById("contacto3").innerHTML = "Email"
+        elIdioma = document.getElementById("contacto4").innerHTML = "Message"
+        elIdioma = document.getElementById("contacto5").innerHTML = "Send Message"
+        elIdioma = document.getElementById("footer1").innerHTML = "Name"
+        elIdioma = document.getElementById("footer2").innerHTML = "Name"
+        elIdioma = document.getElementById("footer3").innerHTML = "Name"
+        elIdioma = document.getElementById("footer4").innerHTML = "Name"
+        elIdioma = document.getElementById("footer1").innerHTML = "Thank you for your visit"
+        elIdioma = document.getElementById("footer2").innerHTML = "For any suggestion you can send me an email"
+        elIdioma = document.getElementById("footer3").innerHTML = "Follow me"
+        elIdioma = document.getElementById("footer4").innerHTML = "Let us be social"
+        elIdioma = document.getElementById("footer5").innerHTML = "Copyright ©2023 All rights reserved"
     }
     else{
         idioma = "espanol"
@@ -34,5 +53,24 @@ function cambioIdioma(){
         elIdioma = document.getElementById("sobreMi2").innerHTML = "Soy un apasionado del aprendizaje continuo y disfruto aplicar nuevas tecnologías para llegar a la resolución de un problema."
         elIdioma = document.getElementById("sobreMi3").innerHTML = "Me gustan los nuevos desafíos. Actualmente incursionando en el mundo freelance."
         elIdioma = document.getElementById("proyectos1").innerHTML = "Proyectos Finalizados"
+        elIdioma = document.getElementById("proyectos2").innerHTML = "Tienda de Vinos. Utilizando tecnologías como ReactJs, CSS, BOOTSTRAP y JavaScript"
+        elIdioma = document.getElementById("proyectos3").innerHTML = "Link a Proyecto"
+        elIdioma = document.getElementById("proyectos4").innerHTML = "Sitio de eventos. Utilizando tecnologías como HTML5, CSS3, BOOTSTRAP and JavaScript"
+        elIdioma = document.getElementById("proyectos5").innerHTML = "Link a Proyecto"
+        elIdioma = document.getElementById("proyectos6").innerHTML = "Juego de memoria desarrollado usando tecnologías como HTML, CSS, BOOTSTRAP y JavaScript"
+        elIdioma = document.getElementById("proyectos7").innerHTML = "Link a Proyecto"
+        elIdioma = document.getElementById("contacto1").innerHTML = "Contactame"
+        elIdioma = document.getElementById("contacto2").innerHTML = "Nombre"
+        elIdioma = document.getElementById("contacto3").innerHTML = "Email"
+        elIdioma = document.getElementById("contacto4").innerHTML = "Mensaje"
+        elIdioma = document.getElementById("contacto5").innerHTML = "Enviar mensaje"
+        elIdioma = document.getElementById("footer1").innerHTML = "Gracias por tu visita"
+        elIdioma = document.getElementById("footer2").innerHTML = "Por cualquier sugerencia puedes enviarme un correo"
+        elIdioma = document.getElementById("footer3").innerHTML = "Sigueme"
+        elIdioma = document.getElementById("footer4").innerHTML = "Seamos sociales"
+        elIdioma = document.getElementById("footer5").innerHTML = "Copyright ©2023 Todos los derechos reservados"
+
     }
 }
+
+
